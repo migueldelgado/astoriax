88984150cb6b90b4eae4a062e0290ce5b0d108d9
kajjin <kajjin@msn.com>
2018-01-17 18:52:32 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,10 @@
 export class AppConfig {
+<<<<<<< Updated upstream
     public static API_ENDPOINT = 'http://homestead.test/api/';
     public static API_ENDPOINT_OLD = '/api'
 
 }
+=======
+    public static API_ENDPOINT= 'http://192.168.10.100/api/';
+ }
+>>>>>>> Stashed changes
