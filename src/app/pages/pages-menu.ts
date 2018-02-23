9e55a02bdfb88bb9e3873111c4f308e3aa719bba
Maximiliano Castro <max@axiomzen.co>
2018-02-22 23:26:21 -0300

    fix lint

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -29,7 +29,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
             title: 'Recetas',
             link: '/pages/store/products/recipes',
           },
-        ]
+        ],
       },
       {
         title: 'Inventarios',
@@ -46,7 +46,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
             title: 'Compras',
             link: '/pages/store/inventories/purchases',
           },
-        ]
+        ],
       },
     ],
   },
