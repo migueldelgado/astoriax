1a059ac1d099ace903b57bfa0379918f95d6cfcb
Miguel Delgado <miguelangeldelgado@gmail.com>
2023-01-19 15:58:43 -0300

    casting value from daily sales to number

--- src/app/@core/utils/utils.ts
+++ src/app/@core/utils/utils.ts
@@ -1,7 +1,8 @@
-export function numberWithCommas(number){
-    return number ? number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : 0;
+export function numberWithCommas(number) {
+  return number ? number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.') : 0;
 }
 
 export function valuePrepareFunction(val) {
-    return Number.isInteger(val) ? '$ ' + numberWithCommas(val) : '-';
+  val = Number(val);
+  return Number.isInteger(val) ? '$ ' + numberWithCommas(val) : '-';
 }
